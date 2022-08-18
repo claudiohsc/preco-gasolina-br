f4ab0eac01b4d5987dfb357cd62ef5f7bfd003ab
Claudio Henrique <claudio885henrique@gmail.com>
2022-08-18 00:14:42 -0300

    criando funcoes para df separados

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ import pandas as pd
 preco_df = pd.read_csv('2004-2021.csv')
 
 
+def criaTabela(nome, index):
+  '''
+    Função que retorna uma tabela separada do dataframe original.
+  '''
+  linhas = preco_df.values.tolist()
+  valores = []
+  nome = nome.upper().strip()
+
+  for linha in linhas:
+    valores.append([linha[int(index)]])
+
+  tabela = pd.DataFrame(valores, columns=[nome])
+  return tabela
+
+
+
 def precoEstado(estado):
   '''
     Função que retorna o valores da tabela por Estado.
@@ -11,6 +27,7 @@ def precoEstado(estado):
   estado = estado.upper().strip()
   
   precos_medios = []
+  
   #Laço de repetição para verificar o estado e se o produto é a gasolina comum.
   for linha in range(len(preco_df)):
     if preco_df['ESTADO'][linha] == estado: 
@@ -30,8 +47,8 @@ def precoEstado(estado):
 
   #Transforma o dicionario em um Data Frame.
   tabela_estado = pd.DataFrame(valores_estado)
-  return print(tabela_estado)
-
+  return tabela_estado
+  
 
 def media(dataframe):
   qtd_valores = 0
