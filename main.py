643a3e092b3844455c7c778d47654167af9ab5dd
Claudio Henrique <claudio885henrique@gmail.com>
2022-08-22 14:00:49 -0300

    criando medias anuais

--- main.py
+++ main.py
@@ -1,47 +1,28 @@
 import pandas as pd
+import plotly
+import plotly.express as px
 
-#Transforma o arquivo csv em um dataframe
 preco_df = pd.read_csv('2004-2021.csv')
 
 
-def criaTabela(nome, index):
+def precoEstado(estado, ano):
   '''
-    Função que retorna uma tabela separada do dataframe original.
+    Função que retorna o valores da tabela de um Estado.
   '''
-  linhas = preco_df.values.tolist()
-  valores = []
-  nome = nome.upper().strip()
 
-  for linha in linhas:
-    valores.append([linha[int(index)]])
-
-  tabela = pd.DataFrame(valores, columns=[nome])
-  return tabela
-
-
-
-def precoEstado(estado):
-  '''
-    Função que retorna o valores da tabela por Estado.
-  '''
-  estado = estado.upper().strip()
-  
+  estado = estado.upper().strip() #Elimina os espaços e coloca tudo em maiúsculo.
   precos_medios = []
-  
-  #Laço de repetição para verificar o estado e se o produto é a gasolina comum.
-  for linha in range(len(preco_df)):
-    if preco_df['ESTADO'][linha] == estado:
-      if preco_df['PRODUTO'][linha] == 'GASOLINA COMUM':
-          precos_medios.append(preco_df['PREÇO MÉDIO REVENDA'][linha]) #adiciona a linha atual depois de passar pelas condições.
-
   datas_finais = []
-
+  
+  #Laço de repetição para verificar o estado, se o produto é a gasolina comum e se a data é igual ao ano passado.
   for linha in range(len(preco_df)):
     if preco_df['ESTADO'][linha] == estado:
       if preco_df['PRODUTO'][linha] == 'GASOLINA COMUM':
         data = preco_df['DATA FINAL'][linha]
-        if data.startswith("2010"):
+        if data[:4] == str(ano): 
+          precos_medios.append(preco_df['PREÇO MÉDIO REVENDA'][linha]) #adiciona a linha atual depois de passar pelas condições.
           datas_finais.append(preco_df['DATA FINAL'][linha])
+          
 
   #Cria um dicionario com todos os valores das listas.
   valores_estado = {'DATA': datas_finais,
@@ -62,10 +43,25 @@ def media(dataframe):
     
   media = soma_preco / qtd_valores
   
-  return print(media)
+  return media
+
+#criando listas para armezenar os anos e as médias
+lista_de_anos = []
+lista_de_medias = []
+
+for ano in range(2004, 2022):
+  media_anual = 0
+  df_estado = precoEstado('DISTRITO FEDERAL', ano) #Cria a tabela de acordo com o ano e o Estado.
+  media_anual = media(df_estado)
+
+  lista_de_anos.append(ano) #adiciona os anos em uma lista.
+  lista_de_medias.append(media_anual) #adiciona as medias em uma lista.
+
+medias_por_ano = {'ANO': lista_de_anos,
+                 'PREÇO MÉDIO REVENDA': lista_de_medias}
+
+tabela_completa = pd.DataFrame(medias_por_ano) #cria um dataframe a partir do dicionario.
 
 
-tabela_estado = precoEstado('DISTRITO FEDERAL')
-
-print(f'A media geral: {media(tabela_estado)}')
-print(tabela_estado)
+fig = px.line(tabela_completa, x='ANO', y='PREÇO MÉDIO REVENDA', title='Preços Médios de Revenda da Gasolina Comum no Distrito Federal.')
+fig.show()
